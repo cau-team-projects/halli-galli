d4ca0972d882043299e40348401385b524a6330e
Byeonggon Lee <gonny952@gmail.com>
2020-11-14 10:56:09 +0900

    Add socket.io

---
+++ client-server/src/index.js
@@ -0,0 +1,37 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const serialport = require('serialport');
+
+const SERIAL_PORT_PATH = '/dev/ttyACM0';
+const SERIAL_PORT_BAUD_RATE = 9600;
+
+const serialParser = new serialport.parsers.Readline('\r\n');
+
+const sp = new serialport(SERIAL_PORT_PATH, {
+  baudRate: SERIAL_PORT_BAUD_RATE,
+});
+
+sp.on('open', () => {
+  console.log('open');
+})
+
+serialParser.on('data', (data) => console.log(data));
+
+sp.pipe(serialParser);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on("disconnect", () => {
+    console.info(`a user disconnected [id=${socket.id}]`);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening...');
+});
