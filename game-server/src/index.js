be19b4927114bd72afddd4dc31a289a88b8c4d4d
Byeonggon Lee <gonny952@gmail.com>
2020-11-14 21:00:05 +0900

    Add switchRoom

--- game-server/src/index.js
+++ game-server/src/index.js
@@ -4,31 +4,48 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
-const rooms = ['home', 'waiting', 'gaming'];
+const HOME_ROOM = 'home';
+const WAITING_ROOM = 'waiting';
+const GAMING_ROOM = 'gaming';
 const users = {}
+
+function switchRoom(socket, newRoom) {
+  const user = users[socket.id];
+  const oldRoom = user.room;
+  if (oldRoom) {
+    socket.leave(oldRoom)
+    io.to(oldRoom).emit('leave', socket.id);
+    console.log(`user ${socket.id} leaves room ${oldRoom}`);
+  }
+  user.room = newRoom;
+  socket.join(newRoom);
+  io.to(newRoom).emit('join', socket.id);
+  console.log(`user ${socket.id} joins room ${newRoom}`);
+}
 
 io.on('connection', (socket) => { 
 
   console.log(`user ${socket.id} connected`);
+  users[socket.id] = {};
 
-  const room = rooms[0];
-  socket.join(room);
-  console.log(`user ${socket.id} joins room ${room}`);
-  users[socket.id] = { room };
+  switchRoom(socket, HOME_ROOM)
+
   console.log(users)
-  io.to(room).emit('join', socket.id);
 
   socket.on('button', (button) => {
     console.log(`button ${button} clicked`);
+    const user = users[socket.id];
+    if (user.room == HOME_ROOM) { /* DO SOMETHING */ }
   });
 
   socket.on('disconnect', () => {
     console.log(`user ${socket.id} disconnected`);
-    const room = rooms[0];
+    const user = users[socket.id];
+    const room = user.room;
     socket.leave(room);
-    delete users[socket.id];
     console.log(`user ${socket.id} leaves room ${room}`);
     io.to(room).emit('leave', socket.id);
+    delete users[socket.id];
   });
 });
 
