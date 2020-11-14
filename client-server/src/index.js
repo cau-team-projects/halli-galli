1b23016ea4d6a81a9cb7b6a97e8c89e7cc108be2
Byeonggon Lee <gonny952@gmail.com>
2020-11-14 11:21:08 +0900

    Periodic emit, user connection limit

--- client-server/src/index.js
+++ client-server/src/index.js
@@ -13,6 +13,8 @@ const sp = new serialport(SERIAL_PORT_PATH, {
   baudRate: SERIAL_PORT_BAUD_RATE,
 });
 
+let user = null;
+
 sp.on('open', () => {
   console.log('open');
 })
@@ -27,11 +29,25 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
   socket.on("disconnect", () => {
     console.info(`a user disconnected [id=${socket.id}]`);
   });
+
+  if (io.engine.clientsCount > 1) {
+    socket.emit('err', { message: 'Only one user can connect' })
+    socket.disconnect()
+  }
+
+  user = socket;
 });
 
 http.listen(3000, () => {
   console.log('listening...');
 });
+
+setInterval(() => {
+  if (user)
+    user.emit('test', Math.random());
+}, 1000);
+
