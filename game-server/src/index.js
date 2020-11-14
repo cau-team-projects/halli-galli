93401575fb5f71b22af4032a261fcd5c2db0eb01
Byeonggon Lee <gonny952@gmail.com>
2020-11-14 19:00:14 +0900

    Room join and leave

--- game-server/src/index.js
+++ game-server/src/index.js
@@ -4,11 +4,28 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
+const rooms = ['room1'];
+
 io.on('connection', (socket) => { 
-  socket.on('button', (btn) => {
-    console.log(`button ${btn} clicked`);
+
+  console.log(`user ${socket.id} connected`);
+
+  const room = rooms[0];
+  socket.join(room);
+  console.log(`user ${socket.id} joins room ${room}`);
+  io.to(room).emit('join', socket.id);
+
+  socket.on('button', (button) => {
+    console.log(`button ${button} clicked`);
   });
-  console.log('connected!');
+
+  socket.on('disconnect', () => {
+    console.log(`user ${socket.id} disconnected`);
+    const room = rooms[0];
+    socket.leave(room);
+    console.log(`user ${socket.id} leaves room ${room}`);
+    io.to(room).emit('leave', socket.id);
+  });
 });
 
 server.listen(4000, () => {
--- relay-server/src/index.js
+++ relay-server/src/index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 let user = null;
 io.on('connection', (socket) => {
   if (io.engine.gamesCount > 1) {
-    socket.emit('err', { message: 'Only one user can connect' })
+    socket.emit('err', { message: 'only one user can connect' })
     socket.disconnect()
   }
 
@@ -21,28 +21,28 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 
   // https://socket.io/docs/client-connection-lifecycle/
-  const game = ioc(`http://localhost:4000`);
-  game.on('connect', () => console.log('connected to game server'));
-  game.on('connect_error', () => console.log('failed to connect to game server'));
-  game.on('disconnect', () => console.log('disconnected from game server'));
-
-  const sp = new serialport(config.SERIAL_PORT_PATH, {
-    baudRate: config.SERIAL_PORT_BAUD_RATE,
-  });
-  sp.on('open', () => {})
-  serialParser.on('data', (data) => {
-    console.log('button clicked');
-    game.emit('button', 0);
-  });
-  sp.pipe(serialParser);
-
-  socket.on("disconnect", () => {
-    console.info(`a user disconnected [id=${socket.id}]`);
-    sp.close((err) => {
-      if (err)
-        console.log(`serial error: ${err}`);
+  const game = ioc(`http://localhost:4000`, { forceNew: true });
+  game.on('connect', () => {
+    console.log('connected to game server')
+    const sp = new serialport(config.SERIAL_PORT_PATH, {
+      baudRate: config.SERIAL_PORT_BAUD_RATE,
+    });
+    sp.on('open', () => {})
+    serialParser.on('data', (button) => {
+      console.log(`button ${button} clicked`);
+      game.emit('button', button);
+    });
+    sp.pipe(serialParser);
+    socket.on("disconnect", () => {
+      console.info(`user ${socket.id} disconnected`);
+      sp.close((err) => {});
+    });
+    game.on('disconnect', () => {
+      console.log('disconnected from game server')
+      sp.close((err) => {});
     });
   });
+  game.on('connect_error', () => console.log('failed to connect to game server'));
 });
 
 http.listen(config.WS_PORT, () => {
