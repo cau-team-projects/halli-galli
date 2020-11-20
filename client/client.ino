1f56956f0a344d1552673993f1fafdff3298e608
Son Kyeonghwan <snapperbay4453@gmail.com>
2020-11-20 21:46:54 +0900

    Apply solving chattering problem of arduino buttons

--- client/client.ino
+++ client/client.ino
@@ -1,35 +1,50 @@
 #define BTN_A 2
 #define BTN_B 3
 #define BTN_X 4
+#define DEBOUNCE_DELAY 150
 
 #include <PinChangeInterrupt.h>
 
-bool btnAFlag = false;
-bool btnBFlag = false;
-bool btnXFlag = false;
+typedef struct _Button {
+  bool state;
+  unsigned long lastDebounceTime;
+} Button;
 
-void btnAInterrupt() {
-  if(!btnAFlag) {
-    btnAFlag = true;
-    Serial.write("A\n");
+Button buttonA = {false, 0};
+Button buttonB = {false, 0};
+Button buttonX = {false, 0};
+
+void buttonAInterrupt() {
+  if(buttonA.state == false) {
+    if ((millis() - buttonA.lastDebounceTime) > DEBOUNCE_DELAY) {
+      buttonA.lastDebounceTime = millis();
+      buttonA.state = true;
+      Serial.write("A\n");
+    }
   }
-  else btnAFlag = false;
+  else buttonA.state = false;
 }
 
-void btnBInterrupt() {
-  if(!btnBFlag) {
-    btnBFlag = true;
-    Serial.write("B\n");
+void buttonBInterrupt() {
+  if(buttonB.state == false) {
+    if ((millis() - buttonB.lastDebounceTime) > DEBOUNCE_DELAY) {
+      buttonB.lastDebounceTime = millis();
+      buttonB.state = true;
+      Serial.write("B\n");
+    }
   }
-  else btnBFlag = false;
+  else buttonB.state = false;
 }
 
-void btnXInterrupt() {
-  if(!btnXFlag) {
-    btnXFlag = true;
-    Serial.write("X\n");
+void buttonXInterrupt() {
+  if(buttonX.state == false) {
+    if ((millis() - buttonX.lastDebounceTime) > DEBOUNCE_DELAY) {
+      buttonX.lastDebounceTime = millis();
+      buttonX.state = true;
+      Serial.write("X\n");
+    }
   }
-  else btnXFlag = false;
+  else buttonX.state = false;
 }
 
 void setup() {
@@ -37,9 +52,9 @@ void setup() {
   pinMode(BTN_A, INPUT);
   pinMode(BTN_B, INPUT);
   pinMode(BTN_X, INPUT);
-  attachPCINT(digitalPinToPCINT(BTN_A), btnAInterrupt, HIGH);
-  attachPCINT(digitalPinToPCINT(BTN_B), btnBInterrupt, HIGH);
-  attachPCINT(digitalPinToPCINT(BTN_X), btnXInterrupt, HIGH);
+  attachPCINT(digitalPinToPCINT(BTN_A), buttonAInterrupt, HIGH);
+  attachPCINT(digitalPinToPCINT(BTN_B), buttonBInterrupt, HIGH);
+  attachPCINT(digitalPinToPCINT(BTN_X), buttonXInterrupt, HIGH);
 
 }
 
