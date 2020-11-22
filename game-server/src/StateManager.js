faa388e2d8725a112be5f7ad4e9ed56a0e244cd5
Byeonggon Lee <gonny952@gmail.com>
2020-11-22 21:02:09 +0900

    Update StateManager

--- game-server/src/StateManager.js
+++ game-server/src/StateManager.js
@@ -16,21 +16,36 @@ module.exports = class StateManager {
 
   replace(newState) {
     const state = this.prepareState(newState);
+    const oldState = this.states[this.states.length - 1];
+
     this.states[this.states.length - 1] = state;
-    if(state.onEnabled)
+
+    if (oldState.onDestroyed)
+      oldState.onDestroyed();
+    if (oldState.onDisabled)
+      oldState.onDisable();
+    if (state.onEnabled)
       state.onEnabled();
   }
 
   push(newState) {
     const state = this.prepareState(newState);
+    const oldState = this.states[this.states.length - 1];
+
     this.states.push(state);
-    if(state.onEnabled)
+
+    if (oldState.onDisabled)
+      oldState.onDisabled();
+    if (state.onEnabled)
       state.onEnabled();
   }
 
   pop(size = 1) {
     for (let i = 0; i < size; ++i) {
       const state = this.states.pop();
+
+      if (oldState.onDestroyed)
+        oldState.onDestroyed();
       if (state.onDisabled)
         state.onDisabled();
     }
