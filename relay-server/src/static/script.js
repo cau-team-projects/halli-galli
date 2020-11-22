55e293a320dd8f69264373aaf43d139b1dd6a9d3
Sudal <kimkusyeon@gmail.com>
2020-11-22 20:03:34 +0900

    경로 수정

--- relay-server/src/static/script.js
+++ relay-server/src/static/script.js
@@ -6,25 +6,25 @@ items: [
 name: "STRAWBERRY",
 number: "1",
 score: 100,
-card: "image/strawberry_1.svg"
+card: "static/image/strawberry_1.svg"
 },
 {
 name: "LEMON",
 number: "2",
 score: 200,
-card: "image/strawberry_2.svg"
+card: "static/image/strawberry_2.svg"
 },
 {
 name: "PEAR",
 number: "3",
 score: 300,
-card: "image/strawberry_3.svg"
+card: "static/image/strawberry_3.svg"
 },
 {
 name: "PINEAPPLE",
 number: "4",
 score: 400,
-card: "image/strawberry_4.svg"
+card: "static/image/strawberry_4.svg"
 }
 ]
 };
