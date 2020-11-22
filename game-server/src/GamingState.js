a91096875f4f91986ce15ccfaf3f57e72e9246a5
Son Kyeonghwan <snapperbay4453@gmail.com>
2020-11-22 19:58:18 +0900

    Add Gaming.js

--- game-server/src/GamingState.js
+++ game-server/src/GamingState.js
@@ -1,17 +1,16 @@
 const constant = require('./constant');
 const State = require('./State');
 const ExitDialogState = require('./ExitDialogState');
-const switchRoom = require('./switchRoom');
 
-module.exports = class MyTurnState extends State {
+module.exports = class GamingState extends State {
   constructor() {
-    super(constant.state.MY_TURN);
+    super(constant.state.GAMING);
     this.on(constant.event.BUTTON_CLICKED, (button) => {
-      if (button == 'A') {
+      if (button === 'A') {
         // flip();
-      } else if (button == 'B') {
+      } else if (button === 'B') {
         // ring();
-      } else if (button == 'X') {
+      } else if (button === 'X') {
         this.push(new ExitDialogState());
       }
     });
