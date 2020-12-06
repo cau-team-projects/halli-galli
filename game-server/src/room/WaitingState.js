f72bf41953b4c1365e529474b943d68e7ae67489
Kyeonghwan <snapperbay4453@gmail.com>
2020-12-06 15:13:51 +0900

    Add countdown stopping feature

--- game-server/src/room/WaitingState.js
+++ game-server/src/room/WaitingState.js
@@ -15,21 +15,27 @@ module.exports = class WaitingState extends State {
         this.start = this.start ?? Date.now();
         const now = Date.now();
         const countdown = 9 - (Math.floor((now - this.start) / 1000) % 10);
-        if (countdown === 0) {
-          this.ready = false;
-          for (const user of users)
-            user.stateManager.push(new UserGamingState());
-          this.push(new RoomGamingState());
-          this.room.emit(
-            constant.event.PAGE_CHANGED,
-            constant.page.GAMING
-          );
+        if (users.length > 0 && users.every((user) => user.stateManager.peek().ready)) {
+          if (countdown === 0) {
+            this.ready = false;
+            for (const user of users)
+              user.stateManager.push(new UserGamingState());
+            this.push(new RoomGamingState());
+            this.room.emit(
+              constant.event.PAGE_CHANGED,
+              constant.page.GAMING
+            );
+          } else {
+            this.room.emit(
+              constant.event.WAITING_COUNTDOWN,
+              countdown
+            );
+          }
         } else {
-          this.room.emit(
-            constant.event.WAITING_COUNTDOWN,
-            countdown
-          );
+          this.ready = false;
+          this.start = null;
         }
+
       } else {
         if (users.length > 0 && users.every((user) => user.stateManager.peek().ready))
           this.ready = true;
