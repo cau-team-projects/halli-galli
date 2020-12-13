0bcbb5e773a5c64bf5e4d4eb8c44b56388b8bced
Sudal <kimkusyeon@gmail.com>
2020-12-13 07:04:33 -0800

    front fix

--- relay-server/src/static/main.js
+++ relay-server/src/static/main.js
@@ -20,11 +20,6 @@ function main() {
       const source = $("#wait_user").html();
       const template = Handlebars.compile(source);
       
-      $('.wait_user li:nth-child(1) .ready .ready_icon').attr("src","static/image/me1.svg");
-      $('.wait_user li:nth-child(2) .ready .ready_icon').attr("src","static/image/me2.svg");
-      $('.wait_user li:nth-child(3) .ready .ready_icon').attr("src","static/image/me3.svg");
-      $('.wait_user li:nth-child(4) .ready .ready_icon').attr("src","static/image/me4.svg");
-      
       socket.on('WAITING_USERS', (users) => {
         for (const user of users) {
           user.isSelf = user.id === selfId;
