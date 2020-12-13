3078a09a487b8d51f9641134becef70d4c2508df
Byeonggon Lee <gonny952@gmail.com>
2020-12-13 23:16:20 +0900

    Fix gaining cards

--- game-server/src/room/GamingState.js
+++ game-server/src/room/GamingState.js
@@ -65,9 +65,10 @@ module.exports = class WaitingState extends State {
         if (cardsFive) {
           // player rung correctly!
           const wonCards = [];
-          for (const otherUser of otherUsers) {
-            this.room.emit(constant.event.GAMING_CARD_LOST, otherUser.id, 1);
-            wonCards.push(otherUser.state.frontCards.splice(0, otherUser.state.frontCards.length));
+          for (const user of users) {
+            const frontCardsLength = user.state.frontCards.length;
+            this.room.emit(constant.event.GAMING_CARD_LOST, user.id, frontCardsLength);
+            wonCards.push(user.state.frontCards.splice(0, frontCardsLength));
           }
           firstRungUser.state.backCards.push(...wonCards);
           this.room.emit(constant.event.GAMING_CARD_GAINED, firstRungUser.id, wonCards.length);
